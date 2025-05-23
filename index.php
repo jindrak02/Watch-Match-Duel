c8eb7da3cbda0d08bffdbf68b077efd84546ff31
Jindrich Krupka <kruj08@vse.cz>
2025-05-23 17:05:39 +0200

    style: UI styling and visual identity created

--- index.php
+++ index.php
@@ -14,18 +14,28 @@ $content = $stmt->fetchAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="public/css/styles.css">
+    <link rel="icon" href="public/images/icon.png" type="image/png">
     <title>WatchMatch Duel</title>
 </head>
-<body class="bg-light">
+<body>
 
     <div class="d-flex flex-column min-vh-100">
         <?php include 'includes/header.html' ?>
 
         <div class="container flex-grow-1 d-flex flex-column justify-content-center align-items-center">
-            <div class="text-center">
-                <h1 class="mb-4">Welcome to WatchMatch Duel!</h1>
-                <p class="lead mb-5">Not sure what to watch tonight? Get your partner or friends and find out!</p>
-                <a href="#" class="btn btn-primary btn-lg" id="startBtn">Start</a>
+            <div class="card border-0 p-4 mt-5 mb-5" style="max-width: 480px; width: 100%;">
+                <div class="text-center">
+                    <h1 class="mb-4">Welcome to <span style="color: var(--color-highlight);">WatchMatch Duel!</span></h1>
+                    <p class="lead mb-5">
+                        Not sure what to watch tonight?<br>
+                        <span style="color: var(--color-highlight); font-weight: 600;">Get your partner or friends and find out!</span>
+                    </p>
+                    <a href="#" class="btn btn-primary btn-lg px-5" id="startBtn">Start</a>
+                    <div class="mt-4" style="font-size: 0.98rem; color: var(--color-text-secondary);">
+                        Discover, match, and enjoy movies &amp; series together.<br>
+                        <span>It's fun, fast, and free!</span>
+                    </div>
+                </div>
             </div>
         </div>
 
