cd5d0e703a24ad376201a04cf0c9c81965c35af8
Jindrich Krupka <kruj08@vse.cz>
2025-05-24 17:56:38 +0200

    feat: code generating to join duel session complete

--- duel.php
+++ duel.php
@@ -2,19 +2,60 @@
 session_start();
 require_once 'includes/db.php';
 
+$code = $_SESSION['code_to_connect'] ?? null;
+
+if (!$code) {
+    $error = "No code found. Please start a new duel.";
+}
 
 ?>
 
-<ul>
-    <li>
-        <?php echo htmlspecialchars($_SESSION['selected_type']) ?>
-    </li>
-    <ul>
-        <?php foreach ($_SESSION['selected_genres'] as $genre): ?>
-            <li><?php echo htmlspecialchars($genre); ?></li>
-        <?php endforeach; ?>
-    </ul>
-    <li>
-        <?php echo htmlspecialchars($_SESSION['selected_duel_count']) ?>
-    </li>
-</ul>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/styles.css">
+    <link rel="icon" href="public/images/icon.png" type="image/png">
+    <title>Start a Duel - WatchMatch Duel</title>
+</head>
+
+<body>
+
+    <div class="d-flex flex-column min-vh-100">
+        <?php include 'includes/header.html' ?>
+
+        <div class="container flex-grow-1 d-flex flex-column justify-content-center align-items-center" id="app">
+
+            <?php if(!empty($error)): ?>
+                <div class="alert alert-danger">
+                    <?php echo htmlspecialchars($error); ?>
+                </div>
+            <?php endif; ?>
+
+            <div class="card border-0 p-4 mt-5 mb-5 slide-right" id="wm-welcome-card">
+                <div class="text-center">
+                    <h1 class="mb-4">Your <span style="color: var(--color-highlight);">code</span> to connect is</span></h1>
+                    <p class="lead mb-5" style="color: var(--color-highlight);">
+                        <?php echo htmlspecialchars($code); ?>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+        <?php include 'includes/footer.html' ?>
+    </div>
+
+
+
+
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</body>
+
+</html>
